ee9969d802b1f7df18236d07a9c806eef18ac698
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-07-31 16:34:14 -0400

    more bug fixes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   Tag.findAll({
-    attributes: ['tag_name'],
+    attributes: ['id', 'tag_name'],
     include: {
       model: Product,
       attributes: ["id", "product_name", "price", "stock", "category_id"],
